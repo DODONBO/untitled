f34233a62fb12aa941adefb4ea30aa46abc6e1a9
DODONBO <109200669+DODONBO@users.noreply.github.com>
2024-03-18 14:39:03 +0900

    Initial commit

---
+++ components/header/container/headerContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <header id="header">
+    <div class="inner">
+      <a href="#" class="logo">LOGO</a>
+      <MainGnb/>
+      <div class="user_menu_container">
+        <div>회원가입</div>
+        <div class="write_btn">글쓰기</div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import {Component, Vue} from 'vue-property-decorator'
+import MainGnb from "~/components/header/mainGnb.vue";
+
+@Component({
+  components: { MainGnb }
+})
+export default class HeaderContainer extends Vue {
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <HeaderContainer/>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import HeaderContainer from '~/components/header/container/headerContainer.vue';
+
+import 'assets/css/style.css';
+
+@Component({
+  components: {HeaderContainer}
+})
+export default class Index extends Vue {
+
+}
+</script>
